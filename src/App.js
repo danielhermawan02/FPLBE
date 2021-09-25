eddfcc3ca6c834a20cbc5ae8b9f8857acd9e3ddf
danielhermawan02 <daniel.hermawan02@gmail.com>
2021-09-26 00:12:18 +0700

    feat: adding router and button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from "react";
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  Switch,
+} from "react-router-dom";
 
 function App() {
+  const [fact,setFact] = useState("");
+  const fetchData = () => {
+    fetch("https://asli-fun-fact-api.herokuapp.com/").then((res) => res.json()).then((data) => setFact(data.data.fact));
+  }
+  useEffect(() => {
+    fetchData();
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+        <h1>FAN FAK ASIK!</h1>
+      {fact&&<p>{fact}</p>}
+      <button onClick = {fetchData}>AAAAAAAHHHH</button>
+      <br/><br/>
+      <Link to="/gaknemuhiks">
+      <button>COBA KLIK</button>
+      </Link>  
+        </Route>
+        <Route path="/profile" exact><h1>Hai aku profile</h1></Route>
+        <Route path="*"><h1>aku 404</h1></Route>
+      </Switch>
+    </BrowserRouter>
     </div>
   );
 }
